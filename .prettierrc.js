4fe8b0a5b9b78a7e07daf7714ad15ed7bab54398
ligoushi <184078718@qq.com>
2021-09-30 00:24:12 +0800

    init pro

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    trailingComma: 'all',// 行尾使用逗号
+    tabWidth: 2,// 缩进
+    semi: false,// 不使用分号
+    singleQuote: true,// 单引号
+    arrowParents: 'always',// 箭头函数总是2使用括号
+    printWidth: 120
+};
