6416f27856f1f19408ff09102174e398a5627b5c
ligoushi <184078718@qq.com>
2021-10-07 17:14:34 +0800

    auto import components

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,11 @@ const app = createApp(App)
 app.use(router)
 app.use(store, key)
 app.use(elementPlus)
+// 自动注册全局组件
+// 这个是vite自己的功能，非web标准或es标准
+const modules = import.meta.globEager('./components/**/index.ts')
+for (const path in modules) {
+  app.use(modules[path].default)
+}
+
 app.mount('#app')
