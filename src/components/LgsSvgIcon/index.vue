f0c70998f5b6e886ec4cefb839565b67375df213
ligoushi <184078718@qq.com>
2022-03-12 20:22:17 +0800

    。

---
+++ src/components/LgsSvgIcon/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <svg :class="svgClass" aria-hidden="true">
+    <use :xlink:href="iconName"></use>
+  </svg>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SvgIcon',
+  props: {
+    iconClass: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String,
+      default: 'svg-icon'
+    }
+  },
+  setup (props) {
+    const iconName = computed(() => {
+      return `#icon-${props.iconClass}`
+    })
+    const svgClass = computed(() => {
+      if (props.className) {
+        return 'svg-icon ' + props.className
+      } else {
+        return 'svg-icon'
+      }
+    })
+    return {
+      iconName,
+      svgClass
+    }
+  }
+})
+</script>
+
+<style scoped>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
